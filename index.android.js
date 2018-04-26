c0b7dce22e1223e9d4998608e4c70fe0213b45e5
Rasmus Faddersbøll <rfader@users.noreply.github.com>
2018-04-26 10:07:05 +0200

    Adds getDetailedStepCountSamples

--- index.android.js
+++ index.android.js
@@ -80,6 +80,34 @@ class RNGoogleFit {
             }
         );
     }
+    
+    /**
+     * Get the detailed steps over a specified date range.
+     * @param {Function} callback The function will be called with an array of elements.
+     */
+
+    getDetailedStepCountSamples = (options, callback) => {
+        let startDate = options.startDate != undefined ? Date.parse(options.startDate) : (new Date()).setHours(0,0,0,0);
+        let endDate = options.endDate != undefined ? Date.parse(options.endDate) : (new Date()).valueOf();
+        let bucketMinutes = options.bucketMinutes != undefined ? parseInt(options.bucketMinutes) : (12 * 60);
+
+        googleFit.getDailyStepCountSamples(startDate, endDate, bucketMinutes,
+            msg => callback(msg, false),
+            (res) => {
+              if (res.length>0) {
+                  callback(false, res.map(function(dev) {
+                          let obj = {};
+                          obj.source = dev.source.appPackage + ((dev.source.stream) ? ":" + dev.source.stream : "");
+                          obj.steps = dev.steps; // this.buildDailySteps(dev.steps);
+                          return obj;
+                      }, this)
+                  );
+              } else {
+                  callback("There is no any steps data for this period", false);
+              }
+            }
+        );
+    }
 
     buildDailySteps(steps) {
         let results = {};
